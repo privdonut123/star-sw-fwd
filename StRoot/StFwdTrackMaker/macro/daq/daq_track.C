8a2f3c5e1e0158edf0d8347c542f4ae5825988e0
Daniel Brandenburg <dbrandenburg.ufl@gmail.com>
2022-05-17 12:16:49 -0400

    Update README and daq_track.C to use 'fwdTrack' BFC option

--- StRoot/StFwdTrackMaker/macro/daq/daq_track.C
+++ StRoot/StFwdTrackMaker/macro/daq/daq_track.C
@@ -14,7 +14,7 @@ void daq_track(    int n = 1,
     gSystem->Load( "libStarRoot.so" );
 
     // Chain with fst, fcs and ftt
-    _chain = Form("in, %s, db, StEvent, MuDST, fcs, fst, ftt", _geom.Data());
+    _chain = Form("in, %s, db, StEvent, MuDST, fcs, fst, ftt, fwdTrack", _geom.Data());
     
     // needed in this wonky spack environment 
     gROOT->SetMacroPath(".:/star-sw/StRoot/macros:./StRoot/macros:./StRoot/macros/graphics:./StRoot/macros/analysis:./StRoot/macros/test:./StRoot/macros/examples:./StRoot/macros/html:./StRoot/macros/qa:./StRoot/macros/calib:./StRoot/macros/mudst:/afs/rhic.bnl.gov/star/packages/DEV/StRoot/macros:/afs/rhic.bnl.gov/star/packages/DEV/StRoot/macros/graphics:/afs/rhic.bnl.gov/star/packages/DEV/StRoot/macros/analysis:/afs/rhic.bnl.gov/star/packages/DEV/StRoot/macros/test:/afs/rhic.bnl.gov/star/packages/DEV/StRoot/macros/examples:/afs/rhic.bnl.gov/star/packages/DEV/StRoot/macros/html:/afs/rhic.bnl.gov/star/packages/DEV/StRoot/macros/qa:/afs/rhic.bnl.gov/star/packages/DEV/StRoot/macros/calib:/afs/rhic.bnl.gov/star/packages/DEV/StRoot/macros/mudst:/afs/rhic.bnl.gov/star/ROOT/36/5.34.38/.sl73_x8664_gcc485/rootdeb/macros:/afs/rhic.bnl.gov/star/ROOT/36/5.34.38/.sl73_x8664_gcc485/rootdeb/tutorials");
@@ -41,17 +41,16 @@ void daq_track(    int n = 1,
     gSystem->Load("libStTpcDb.so");
 
     //  Tracking
-    if ( true ) {
-        StFwdTrackMaker *gmk = new StFwdTrackMaker();
-        gmk->SetConfigFile( configFile );
+    
+    StFwdTrackMaker *fwdTrack = (StFwdTrackMaker*) chain->GetMaker("fwdTrack");
+    if ( fwdTrack ){ //if it is in the chain
+        fwdTrack->SetConfigFile( configFile );
         
         // write debug histograms and ttree?
-        gmk->SetGenerateTree( true );
-        gmk->SetGenerateHistograms( true );
+        fwdTrack->SetGenerateTree( true );
+        fwdTrack->SetGenerateHistograms( true );
         // write out wavefront OBJ files
-        gmk->SetVisualize( true );
-        
-        chain->AddMaker(gmk);
+        fwdTrack->SetVisualize( true );
     }
 
     chain->Init();
